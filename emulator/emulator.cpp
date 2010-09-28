da806f7b2e3d38cf32a47a5668ce803b0f2873b2
ken.domino@gmail.com <ken.domino@gmail.com@b05b5d53-d61a-d14b-5d44-457121efbe5e>
2010-09-28 17:16:46 +0000

    Clean up, trying to follow coding standard.
    git-svn-id: http://cuda-waste.googlecode.com/svn/trunk@96 b05b5d53-d61a-d14b-5d44-457121efbe5e
    
    Former-commit-id: 009f36f0ef513535be4d67d64ca510d426d76502 [formerly b0382af3178b88dc1393a1a45a1f3eb95c57dda8] [formerly c34d0865224b65c59c6aa988ebdf78273f526834 [formerly bcfa991d336f01af7d596ae3350e165e57835ffb]] [formerly 543ec2e580586fb9eb56f68762866ddb3cb57a88 [formerly e95c9022ce89d39ff763e6fd6e369e2e4338d86b] [formerly 67c3fc9aafb0d9f140c3f61edfccb11bbc85ed47]] [formerly 61fe51af74cdd4ec3563488983c698b631626f71 [formerly 2f5f1040454d2fe40d5e3dac6635f218c5ceaf81] [formerly 67c3fc9aafb0d9f140c3f61edfccb11bbc85ed47 [formerly 46e167feb67452895d7c727a4877b97a75e37ff5]]] [formerly 2afab92df3a70c7f268e15e09dad38d72244799b [formerly 79b03c4b8f5e5af259c8aa50a41a00e9c52e97b7] [formerly 7387ecc14bdefc84e470d773f27786e5fba22a53 [formerly d907248e25372546124f5de765f718fa4b1efbff]] [formerly 3f57313e8041e125b9a4aa6af246684b18ce1f41 [formerly 84c0ee23abb8d7ee0a8d2384fc0738d20fc07f8a] [formerly a336e122bd068befae28ce98b163d728b703e016]] [formerly 61fe51af74cdd4ec3563488983c698b631626f71 [formerly 2f5f1040454d2fe40d5e3dac6635f218c5ceaf81] [formerly 67c3fc9aafb0d9f140c3f61edfccb11bbc85ed47 [formerly 46e167feb67452895d7c727a4877b97a75e37ff5]] [formerly 2d845af3af0faf2ec9bd8e95af01804e51a5b70c [formerly f02cad96f9d0d72119ac3789f9e0938ea197eca9] [formerly a336e122bd068befae28ce98b163d728b703e016 [formerly ea350617d703dd22c6544daefa4f3e0c79af4952]]]]]
    Former-commit-id: fb9e7a2469a3b2289bcbc0a6e06060ba2c891ca0 [formerly bc18cae5019b1f88de3a8369e01bc2eb184cdbdd] [formerly a4a39849a4fe9b5e540087bd657cce11f9fc2357 [formerly d980c8c25914e8a9d700015c7abf2585e8e745e1]] [formerly 6b23010d8b9cf91236046bd77ee259739c61e722 [formerly d8eaa2e8d2b65580602d8d9241a940fcd50c66b4] [formerly 2d845af3af0faf2ec9bd8e95af01804e51a5b70c]] [formerly c45c32e67e9c8f9c1b7e916e23fbcfbcd45041d8 [formerly cca813e7bff0c1cdc92abc54d79b04a3743a7365] [formerly 8f3f4df2f3c0bf3d4af37f22fd6e335a40afd02d [formerly b55cb7c8d28e3aa1e26b60f5f8e2cf21781ec9b0]] [formerly 2d845af3af0faf2ec9bd8e95af01804e51a5b70c [formerly f02cad96f9d0d72119ac3789f9e0938ea197eca9]]]
    Former-commit-id: c45c32e67e9c8f9c1b7e916e23fbcfbcd45041d8 [formerly cca813e7bff0c1cdc92abc54d79b04a3743a7365] [formerly 8f3f4df2f3c0bf3d4af37f22fd6e335a40afd02d [formerly b55cb7c8d28e3aa1e26b60f5f8e2cf21781ec9b0]]
    Former-commit-id: b88367c94b5ceb5b1c89d176b1262512054fa370 [formerly 88eacf14a713cf043acfca567aa08dd2c4451b8a] [formerly 8f3f4df2f3c0bf3d4af37f22fd6e335a40afd02d]
    Former-commit-id: a7a7e59a23e56628a2672612b268f0dfebda51f9 [formerly 2c749c51832f85a6938c588b18ef099d5d54579d]
    Former-commit-id: 8faea081c203107c0ca2e8072c111c56daebeec9

--- emulator/emulator.cpp
+++ emulator/emulator.cpp
@@ -1,3 +1,18 @@
+/*
+   Copyright 2010 Ken Domino
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 #include "emulator.h"
 #include <assert.h>
 #include <fstream>
@@ -38,7 +53,7 @@ void EMULATOR::SetTrace(int level)
 // In ptxp/driver.cpp.
 extern TREE * parse(char * source);
 
-TREE * EMULATOR::Extract_From_Source(char * module_name, char * source)
+TREE * EMULATOR::Parse(char * module_name, char * source)
 {
     // Pick modules of only one type.
     if (strstr(module_name, this->device) == 0)
--- emulator/emulator.h
+++ emulator/emulator.h
@@ -1,3 +1,18 @@
+/*
+   Copyright 2010 Ken Domino
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 #pragma once
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
@@ -128,7 +143,7 @@ public:
     static EMULATOR * Singleton();
     
     // Parse
-    TREE * Extract_From_Source(char * module_name, char * source);
+    TREE * Parse(char * module_name, char * source);
     
     // cuda_runtime.h equivalents.
     void ** _cudaRegisterFunction(void * fun, char * name);
