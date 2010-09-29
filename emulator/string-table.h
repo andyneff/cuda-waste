68464b4a6d8c079463882a341af8513c7fa62d69
ken.domino@gmail.com <ken.domino@gmail.com@b05b5d53-d61a-d14b-5d44-457121efbe5e>
2010-09-29 10:50:27 +0000

    Clean up.
    git-svn-id: http://cuda-waste.googlecode.com/svn/trunk@100 b05b5d53-d61a-d14b-5d44-457121efbe5e
    
    Former-commit-id: 8548c8cf1a35393a51537e314ad318caf999d654 [formerly cb587cbaa2681038f5f41f701df88a506fa73445] [formerly aabc9a9aab16bf2c69b150c7c8177135bf2d2b68 [formerly accb0c382531f9e1ef0280935c17453ece6e164f]] [formerly 73e8e5eab65b3bb2217d422bfbb8ff8371638fb0 [formerly 9794ecc436c6e7bcde16613991589979bc6dbe51] [formerly 72a736f4152551e33eafcf7cf17a033b1e5935d0]] [formerly e0fe9204a7de77b49c2056af952bcaabe34f346b [formerly 17fb27caa11453d49f828bd3c32a2d172b55b38e] [formerly 72a736f4152551e33eafcf7cf17a033b1e5935d0 [formerly 2e3090d3a5ae16542cc319c0afc16e96d17ede58]]] [formerly b45c00c80a08329e54490157d5d28ed503c26cb6 [formerly cb81fe46fc8a30aa1b246d440e7c9ed9e1c1e325] [formerly 2f9fe0ddcfd4b4a97a0a3a31b4b590fed359d386 [formerly f27d5417357cb30f88746aef2118667015217ec9]] [formerly 3910517a93425bb096d9565677af60266cb4343a [formerly 17e1bb1f281a2428a6fb75150c47383b6c672f0b] [formerly de9a83edda69912ea76046bb51a4a279c047d4c4]] [formerly e0fe9204a7de77b49c2056af952bcaabe34f346b [formerly 17fb27caa11453d49f828bd3c32a2d172b55b38e] [formerly 72a736f4152551e33eafcf7cf17a033b1e5935d0 [formerly 2e3090d3a5ae16542cc319c0afc16e96d17ede58]] [formerly 81f696a07a7c547b6e750df532216c9ea5794018 [formerly 9c92878d29343a956b327011050ecc15742cf46c] [formerly de9a83edda69912ea76046bb51a4a279c047d4c4 [formerly 81c7a023d6f0982204f73f486769a39995748d86]]]]]
    Former-commit-id: 9e7d1c4667752fd378756def2eb7124ecb023d4e [formerly 7ad3fb216fab1e5fedd132c62a7ba7041a55230f] [formerly 33932c0e95af9b1ba618d7e4b8f46991261a8b92 [formerly f325f5582c377bc0a0337cc7588c91ec8e032397]] [formerly de4564916c3e899dd175d122c0dee0a746d133dc [formerly ba2c2b4db4aa18bb4ba99a5d8fd257b0314828b2] [formerly 81f696a07a7c547b6e750df532216c9ea5794018]] [formerly a6b70385a01aa297b90d5cdc97c1eac5c2efbe18 [formerly a5b640153c4e2e756b99b3c336579d11af9553f7] [formerly b8db417004b9304b23289aa61462709e24677a67 [formerly e702a7ab949e577266132fc58a594212148f22de]] [formerly 81f696a07a7c547b6e750df532216c9ea5794018 [formerly 9c92878d29343a956b327011050ecc15742cf46c]]]
    Former-commit-id: 64bd35f40532159ff736e11e7beab2978cab7bd8 [formerly 1dd330dd5195bb4658e0dfd8d406595c3db81d27] [formerly cda0134a76449968b2c4e4976c1b6fc5666904b4 [formerly 7c1021fb48bd4ee7e1eb50a503f2f9900616a11a]] [formerly a6b70385a01aa297b90d5cdc97c1eac5c2efbe18 [formerly a5b640153c4e2e756b99b3c336579d11af9553f7] [formerly b8db417004b9304b23289aa61462709e24677a67 [formerly e702a7ab949e577266132fc58a594212148f22de]]]
    Former-commit-id: 00b9fb50b284cb1151c90388f2ba187e763da995 [formerly 9243465e7e9842962b01488d113ddeb4f42d7427] [formerly b8db417004b9304b23289aa61462709e24677a67]
    Former-commit-id: 36a51f96aa661ddcd0bb815ba8f1bc01df55cd0d [formerly 4f5a56488ed4629b7efee200c9f8569db8aea95b]
    Former-commit-id: 592bf54019e80c4c0b42e29fbd641fca28305185

--- emulator/string-table.h
+++ emulator/string-table.h
@@ -14,17 +14,7 @@
    limitations under the License.
 */
 #pragma once
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-#include <list>
 #include <map>
-#include "../ptxp/PtxLexer.h"
-//#include "../ptxp/PtxParser.h"
-#include "tree.h"
-#include <cuda.h>
-#include <cuda_runtime.h> // cudaError_t, CUDARTAPI, etc.
-
 
 class STRING_TABLE
 {
--- emulator/tree.h
+++ emulator/tree.h
@@ -19,25 +19,43 @@
 
 class SYMBOL;
 
+// The TREE class is used to represent the abstract syntax tree from
+// the parse of PTX assembly code.  Antlr is used to do the parsing.
+// But, for a number of reasons, I do not want Antlr data structures
+// used in the rest of the CUDA emulator.  Instead, the AST is
+// converted from the Antlr data structure into this data structure.
 class TREE
 {
-public:
-    TREE();
-    int GetType();
-    char * GetText();
-    TREE * GetChild(int index);
-    int GetChildCount();
-    void SetText(char * text);
-    void SetType(int type);
-    void AddChild(TREE * child);
-    SYMBOL * GetSymbol();
-    void SetSymbol(SYMBOL * symbol);
-    TREE * GetParent();
-    void SetParent(TREE * parent);
-private:
-    std::vector<TREE*> children;
-    int type;
-    char * text;
-    SYMBOL * symbol;
-    TREE * parent;
+    private:
+        // A node in the tree has children.
+        std::vector<TREE*> children;
+
+        // Each node has a type.
+        int type;
+
+        // Every node has a text representation.  The text
+        // representation is just for the node itself, not for the
+        // forest.
+        char * text;
+
+        // A node can represent a symbol, e.g., a register.  This field is an
+        // optimization, which points to the symbol data structure.
+        SYMBOL * symbol;
+
+        // The TREE data structure is a general tree.  Every node has a
+        // parent.
+        TREE * parent;
+    public:
+        TREE();
+        int GetType();
+        char * GetText();
+        TREE * GetChild(int index);
+        int GetChildCount();
+        void SetText(char * text);
+        void SetType(int type);
+        void AddChild(TREE * child);
+        SYMBOL * GetSymbol();
+        void SetSymbol(SYMBOL * symbol);
+        TREE * GetParent();
+        void SetParent(TREE * parent);
 };
