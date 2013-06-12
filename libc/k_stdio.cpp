14469c305123deaa9390e6d14b9e4be5407c68d0
Ken.Domino@gmail.com <Ken.Domino@gmail.com@b05b5d53-d61a-d14b-5d44-457121efbe5e>
2013-06-12 03:32:24 +0000

    Changes for: nested blocks; printf in kernels in 64-bit mode; textures/pitch/array manipulation.
    git-svn-id: http://cuda-waste.googlecode.com/svn/trunk@174 b05b5d53-d61a-d14b-5d44-457121efbe5e
    
    Former-commit-id: 1f248c1253840d345a84007c28adbd9dfa68b481 [formerly 31735f86be6eff27bde8542c834cd1b5032d62bd] [formerly 5f5b40aebebd848797ebee8273b0aa0f8071f45f [formerly 0fd47eea1040da35b83f2d11adc65de5777ea11c]] [formerly 86414a7bab9bfaae0d732a14429710a501d5d995 [formerly d63338929de5a22535b421a767cb69420c4c6e45] [formerly 8fa2e796b7d55ce02d643bdebc8f232233b4c0fb]] [formerly d899219b54c899bb543dabbde75ad23b09c57be4 [formerly c57b533854e854d8ce46dbca5b3cba1669360f32] [formerly 8fa2e796b7d55ce02d643bdebc8f232233b4c0fb [formerly 03cce298a53be633d934ff69460768247c8aa59e]]] [formerly 01a0f3ec8d00ad0b8c452b056188bb566951dacc [formerly 679bd1c239c9b712dc1e2725ded34a0550e0dfc0] [formerly f7d1cf97a11b8b51142d4b647a522c02c431ec65 [formerly c0fec8fb3065c760939bb03722903b64ad306fb8]] [formerly d0282eb3bb565d545e7d3e30494fee1cdd6046bd [formerly 95d04012370ffdcd288f302598431cef45167107] [formerly 7c2261950d68ab2c27e40a57b8e7fa2f0a3c100a]] [formerly d899219b54c899bb543dabbde75ad23b09c57be4 [formerly c57b533854e854d8ce46dbca5b3cba1669360f32] [formerly 8fa2e796b7d55ce02d643bdebc8f232233b4c0fb [formerly 03cce298a53be633d934ff69460768247c8aa59e]] [formerly e89511bbc0ab4e2c6a5c10ca3c8a37a9150f52e9 [formerly a228686820e14a20d25e63980b5fefee62c7df34] [formerly 7c2261950d68ab2c27e40a57b8e7fa2f0a3c100a [formerly 502314cc725cd5443b149c0244b18e0fd2095eb9]]]]]
    Former-commit-id: a00af178d1242f75203cb5fe4bf3a02936ed4e66 [formerly e0c7b28d69b80a47c26a4ef772c4e369da7f4a95] [formerly 5a34afdc1726792084126f0198b5c2a4d8bf422c [formerly 40b01eb85c5247f80ee284200bd438249c00530f]] [formerly bf0fb03bf4f7073dd3e61606b2d895d8bd5c16c8 [formerly 65350af919b0b6882d4fad02f9fab9a0da95eabe] [formerly e89511bbc0ab4e2c6a5c10ca3c8a37a9150f52e9]] [formerly b7f473dab8680554c6c60dbb5f18c3a50f3f9bf5 [formerly ab3e1abda12101989ff4dbbe4f89d7965f4e4214] [formerly 390aa4f0f38975e7c246e3cde0106fdd8c97bd5c [formerly bb4b05bb34615d17688b31197cc33f1a014770f3]] [formerly e89511bbc0ab4e2c6a5c10ca3c8a37a9150f52e9 [formerly a228686820e14a20d25e63980b5fefee62c7df34]]]
    Former-commit-id: b7f473dab8680554c6c60dbb5f18c3a50f3f9bf5 [formerly ab3e1abda12101989ff4dbbe4f89d7965f4e4214] [formerly 390aa4f0f38975e7c246e3cde0106fdd8c97bd5c [formerly bb4b05bb34615d17688b31197cc33f1a014770f3]]
    Former-commit-id: 1880037982190b61b94f675ec6fce943d5062441 [formerly f897f9293b7169daf70e721fe6873c8f0f06a13d] [formerly a3eceda1097ee3b37302daf0f4b52c47f48640a7 [formerly 390aa4f0f38975e7c246e3cde0106fdd8c97bd5c]]
    Former-commit-id: 71b9c3f3e2c09b683236c2da34dc5ef1a3d802eb [formerly 6f132d6fc2bc5b9078db7f58a768003388a10934]
    Former-commit-id: 4d53dadc71ee932e4253a0fabadb2dfac4715f07

---
+++ libc/k_stdio.cpp
@@ -0,0 +1,492 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Based on original stdio.c released by Atmel
+ * Copyright (c) 2008, Atmel Corporation
+ * All rights reserved.
+ *
+ * Modified by Roel Verdult, Copyright (c) 2010
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+//------------------------------------------------------------------------------
+//         Headers
+//------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <type_traits>
+
+//------------------------------------------------------------------------------
+//         Local Definitions
+//------------------------------------------------------------------------------
+
+// Maximum string size allowed (in bytes).
+#define MAX_STRING_SIZE         255
+
+//------------------------------------------------------------------------------
+//         Global Variables
+//------------------------------------------------------------------------------
+
+// Required for proper compilation.
+//struct _reent r = {0, (FILE*) 0, (FILE*) 1, (FILE*) 0};
+//struct _reent *_impure_ptr = &r;
+
+//------------------------------------------------------------------------------
+//         Local Functions
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Writes a character inside the given string. Returns 1.
+// \param pStr  Storage string.
+// \param c  Character to write.
+//------------------------------------------------------------------------------
+signed int append_char(char *pStr, char c)
+{
+    *pStr = c;
+    return 1;
+}
+
+//------------------------------------------------------------------------------
+// Writes a string inside the given string.
+// Returns the size of the written
+// string.
+// \param pStr  Storage string.
+// \param pSource  Source string.
+//------------------------------------------------------------------------------
+signed int PutString(char *pStr, char fill, signed int width, const char *pSource)
+{
+    signed int num = 0;
+
+    while (*pSource != 0) {
+
+        *pStr++ = *pSource++;
+        num++;
+    }
+
+        width -= num;
+        while (width > 0) {
+
+        *pStr++ = fill;
+                num++;
+                width--;
+        }
+
+    return num;
+}
+
+//------------------------------------------------------------------------------
+// Writes an unsigned int inside the given string, using the provided fill &
+// width parameters.
+// Returns the size in characters of the written integer.
+// \param pStr  Storage string.
+// \param fill  Fill character.
+// \param width  Minimum integer width.
+// \param value  Integer value.
+//------------------------------------------------------------------------------
+signed int PutUnsignedInt(
+    char *pStr,
+    char fill,
+    signed int width,
+    unsigned int value)
+{
+    signed int num = 0;
+
+    // Take current digit into account when calculating width
+    width--;
+
+    // Recursively write upper digits
+    if ((value / 10) > 0) {
+
+        num = PutUnsignedInt(pStr, fill, width, value / 10);
+        pStr += num;
+    }
+    // Write filler characters
+    else {
+
+        while (width > 0) {
+
+            append_char(pStr, fill);
+            pStr++;
+            num++;
+            width--;
+        }
+    }
+
+    // Write lower digit
+    num += append_char(pStr, (value % 10) + '0');
+
+    return num;
+}
+
+//------------------------------------------------------------------------------
+// Writes a signed int inside the given string, using the provided fill & width
+// parameters.
+// Returns the size of the written integer.
+// \param pStr  Storage string.
+// \param fill  Fill character.
+// \param width  Minimum integer width.
+// \param value  Signed integer value.
+//------------------------------------------------------------------------------
+signed int PutSignedInt(
+    char *pStr,
+    char fill,
+    signed int width,
+    signed int value)
+{
+    signed int num = 0;
+    unsigned int absolute;
+
+    // Compute absolute value
+    if (value < 0) {
+
+        absolute = -value;
+    }
+    else {
+
+        absolute = value;
+    }
+
+    // Take current digit into account when calculating width
+    width--;
+
+    // Recursively write upper digits
+    if ((absolute / 10) > 0) {
+
+        if (value < 0) {
+        
+            num = PutSignedInt(pStr, fill, width, -(absolute / 10));
+        }
+        else {
+
+            num = PutSignedInt(pStr, fill, width, absolute / 10);
+        }
+        pStr += num;
+    }
+    else {
+
+        // Reserve space for sign
+        if (value < 0) {
+
+            width--;
+        }
+
+        // Write filler characters
+        while (width > 0) {
+
+            append_char(pStr, fill);
+            pStr++;
+            num++;
+            width--;
+        }
+
+        // Write sign
+        if (value < 0) {
+
+            num += append_char(pStr, '-');
+            pStr++;
+        }
+    }
+
+    // Write lower digit
+    num += append_char(pStr, (absolute % 10) + '0');
+
+    return num;
+}
+
+//------------------------------------------------------------------------------
+// Writes an hexadecimal value into a string, using the given fill, width &
+// capital parameters.
+// Returns the number of char written.
+// \param pStr  Storage string.
+// \param fill  Fill character.
+// \param width  Minimum integer width.
+// \param maj  Indicates if the letters must be printed in lower- or upper-case.
+// \param value  Hexadecimal value.
+//------------------------------------------------------------------------------
+signed int PutHexa(
+    char *pStr,
+    char fill,
+    signed int width,
+    unsigned char maj,
+    unsigned int value)
+{
+    signed int num = 0;
+
+    // Decrement width
+    width--;
+
+    // Recursively output upper digits
+    if ((value >> 4) > 0) {
+
+        num += PutHexa(pStr, fill, width, maj, value >> 4);
+        pStr += num;
+    }
+    // Write filler chars
+    else {
+
+        while (width > 0) {
+
+            append_char(pStr, fill);
+            pStr++;
+            num++;
+            width--;
+        }
+    }
+
+    // Write current digit
+    if ((value & 0xF) < 10) {
+
+        append_char(pStr, (value & 0xF) + '0');
+    }
+    else if (maj) {
+
+        append_char(pStr, (value & 0xF) - 10 + 'A');
+    }
+    else {
+
+        append_char(pStr, (value & 0xF) - 10 + 'a');
+    }
+    num++;
+
+    return num;
+}
+
+//------------------------------------------------------------------------------
+//         Global Functions
+//------------------------------------------------------------------------------
+
+
+#define _INTSIZEOF(n)   ( (sizeof(n) + sizeof(int) - 1) & ~(sizeof(int) - 1) )
+
+#define my_va_start(ap,v)  ( ap = (va_list)_ADDRESSOF(v) + _INTSIZEOF(v) )
+#define my_va_arg(ap,t)    ( *(t *)((ap += _INTSIZEOF(t)) - _INTSIZEOF(t)) )
+#define my_va_end(ap)      ( ap = (va_list)0 )
+
+//------------------------------------------------------------------------------
+/// Stores the result of a formatted string into another string. Format
+/// arguments are given in a va_list instance.
+/// Return the number of characters written.
+/// \param pStr    Destination string.
+/// \param length  Length of Destination string.
+/// \param pFormat Format string.
+/// \param ap      Argument list.
+//------------------------------------------------------------------------------
+template<class  T>
+signed int k_vsnprintf(char *pStr, size_t length, const char *pFormat, va_list ap)
+{
+    char          fill;
+    unsigned char width;
+    signed int    num = 0;
+    signed int    size = 0;
+
+    // Clear the string
+    if (pStr) {
+
+        *pStr = 0;
+    }
+
+    // Phase string
+    while (*pFormat != 0 && size < length) {
+
+        // Normal character
+        if (*pFormat != '%') {
+
+            *pStr++ = *pFormat++;
+            size++;
+        }
+        // Escaped '%'
+        else if (*(pFormat+1) == '%') {
+
+            *pStr++ = '%';
+            pFormat += 2;
+            size++;
+        }
+        // Token delimiter
+        else {
+
+            fill = ' ';
+            width = 0;
+            pFormat++;
+
+            // Parse filler
+            if (*pFormat == '0') {
+
+                fill = '0';
+                pFormat++;
+            }
+
+            // Ignore justifier
+            if (*pFormat == '-') {
+                pFormat++;
+            }
+
+            // Parse width
+            while ((*pFormat >= '0') && (*pFormat <= '9')) {
+        
+                width = (width*10) + *pFormat-'0';
+                pFormat++;
+            }
+
+            // Check if there is enough space
+            if (size + width > length) {
+
+                width = length - size;
+            }
+        
+			// Parse type
+            switch (*pFormat) {
+            case 'd': 
+            case 'i': num = PutSignedInt(pStr, fill, width, my_va_arg(ap, std::make_signed<T>::type)); break;
+            case 'u': num = PutUnsignedInt(pStr, fill, width, my_va_arg(ap, std::make_unsigned<T>::type)); break;
+            case 'x': num = PutHexa(pStr, fill, width, 0, my_va_arg(ap, std::make_unsigned<T>::type)); break;
+            case 'X': num = PutHexa(pStr, fill, width, 1, my_va_arg(ap, std::make_unsigned<T>::type)); break;
+            case 's': num = PutString(pStr, fill, width, my_va_arg(ap, char *)); break;
+            case 'c': num = append_char(pStr, my_va_arg(ap, std::make_unsigned<T>::type)); break;
+            default:
+                return EOF;
+            }
+
+            pFormat++;
+            pStr += num;
+            size += num;
+        }
+    }
+
+    // NULL-terminated (final \0 is not counted)
+    if (size < length) {
+
+        *pStr = 0;
+    }
+    else {
+
+        *(--pStr) = 0;
+        size--;
+    }
+
+    return size;
+}
+
+//------------------------------------------------------------------------------
+/// Stores the result of a formatted string into another string. Format
+/// arguments are given in a va_list instance.
+/// Return the number of characters written.
+/// \param pString Destination string.
+/// \param length  Length of Destination string.
+/// \param pFormat Format string.
+/// \param ...     Other arguments
+//------------------------------------------------------------------------------
+template<class  T>
+signed int k_snprintf(char *pString, size_t length, const char *pFormat, ...)
+{
+    va_list    ap;
+    signed int rc;
+
+    va_start(ap, pFormat);
+    rc = k_vsnprintf<T>(pString, length, pFormat, ap);
+    va_end(ap);
+
+    return rc;
+}
+
+//------------------------------------------------------------------------------
+/// Stores the result of a formatted string into another string. Format
+/// arguments are given in a va_list instance.
+/// Return the number of characters written.
+/// \param pString  Destination string.
+/// \param pFormat  Format string.
+/// \param ap       Argument list.
+//------------------------------------------------------------------------------
+template<class  T>
+signed int k_vsprintf(char *pString, const char *pFormat, va_list ap)
+{
+    return k_vsnprintf<T>(pString, MAX_STRING_SIZE, pFormat, ap);
+}
+
+//------------------------------------------------------------------------------
+/// Outputs a formatted string on the DBGU stream. Format arguments are given
+/// in a va_list instance.
+/// \param pFormat  Format string
+/// \param ap  Argument list.
+//------------------------------------------------------------------------------
+template<class  T>
+signed int k_vprintf(const char *pFormat, va_list ap)
+{
+  char pStr[MAX_STRING_SIZE];
+  char pError[] = "stdio.c: increase MAX_STRING_SIZE\r\n";
+  
+  // Write formatted string in buffer
+  if (k_vsprintf<T>(pStr, pFormat, ap) >= MAX_STRING_SIZE) {
+    
+    puts(pError);
+    while (1); // Increase MAX_STRING_SIZE
+  }
+  
+  // Display string
+  return puts(pStr);
+}
+
+//------------------------------------------------------------------------------
+/// Outputs a formatted string on the DBGU stream, using a variable number of
+/// arguments.
+/// \param pFormat  Format string.
+//------------------------------------------------------------------------------
+template<class  T>
+signed int k_printf(const char *pFormat, ...)
+{
+    va_list ap;
+    signed int result;
+
+    // Forward call to vprintf
+    va_start(ap, pFormat);
+    result = k_vprintf<T>(pFormat, ap);
+    va_end(ap);
+
+    return result;
+}
+
+
+//------------------------------------------------------------------------------
+/// Writes a formatted string inside another string.
+/// \param pStr  Storage string.
+/// \param pFormat  Format string.
+//------------------------------------------------------------------------------
+template<class  T>
+signed int k_sprintf(char *pStr, const char *pFormat, ...)
+{
+    va_list ap;
+    signed int result;
+
+    // Forward call to vsprintf
+    va_start(ap, pFormat);
+    result = k_vsprintf<T>(pStr, pFormat, ap);
+    va_end(ap);
+
+    return result;
+}
+
+template
+signed int k_vprintf<__int32>(const char *pFormat, va_list ap);
